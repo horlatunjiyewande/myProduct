6df47bf624abaa37a9f974a05a4afa876d4f2deb
Olatunji Yewande <horlatunjiyewande@gmail.com>
2026-09-15 14:07:18 +0100

    updated mongoose connect

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ const express = require("express")
 const mongoose = require('mongoose')
 const productRoute = require("./route/productRoutes")
 const userRoute = require("./route/userRoutes")
-mongoose.connect(compass_string)
-
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("Connection Error; ",err));
-
 
 const app = express()
 const port = 5555
 const compass_string = process.env.COMPASS_STRING
  const atlas_string = process.env.ATLAS_STRING
 
+ mongoose.connect(compass_string)
+ .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("Connection Error; ",err));
+
+
 app.use(express.json())
-
-
 app.get("/",(req,res) =>{
     res.send("Server is active")
 })
